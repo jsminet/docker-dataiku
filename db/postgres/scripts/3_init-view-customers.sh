58aa7657025810138acea82f1ae7218524de2317
JS <jseb.minet@gmail.com>
2024-04-15 14:57:02 +0200

    Update view to cast double to int

--- db/postgres/scripts/3_init-view-customers.sh
+++ db/postgres/scripts/3_init-view-customers.sh
@@ -2,13 +2,13 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-CREATE OR REPLACE VIEW vw_customers as (
-SELECT floor(random()* (100 + 1) + 0) as age,
-row_number() over () as id,
-row_number() over () %2 as group_a,
-row_number() over () %10 as group_b,
-'user'||row_number() over () as username, 
-normal_rand,
-upper(encode(gen_random_bytes(5),'hex')) as password
-FROM normal_rand(1000000, 5, 3));
+CREATE OR REPLACE VIEW vw_customers AS 
+SELECT cast(floor(random() * (100 + 1)+ 0) as integer) AS age,
+    row_number() OVER () AS id,
+    row_number() OVER () % 2 AS group_a,
+    row_number() OVER () % 10 AS group_b,
+    'user' || LPAD((row_number() OVER ())::text, 7, '0') AS username,
+    normal_rand,
+    upper(encode(gen_random_bytes(5), 'hex')) AS password
+   FROM normal_rand(1000000, 5, 3);
 EOSQL
